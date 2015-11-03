be6bb0e3b53ff081567be369041a8ad98614ecc1
Michaël Zasso <mic.besace@gmail.com>
2015-11-03 16:43:00 +0100

    filter with keyword and predicate options

--- src/index.js
+++ src/index.js
@@ -1,5 +1,55 @@
 'use strict';
 
-module.exports = function () {
-    return true;
-};
+module.exports = filter;
+module.exports.match = match;
+
+function filter(array, options) {
+    options = options || {};
+    var result = [];
+    for (var i = 0; i < array.length; i++) {
+        if (match(array[i], options.keywords, options.predicate || 'OR')) {
+            result.push(array[i]);
+        }
+    }
+    return result;
+}
+
+function match(element, keywords, predicate) {
+    var found = false;
+    if (keywords) {
+        for (var i = 0; i < keywords.length; i++) {
+            if (recursiveMatch(element, keywords[i])) {
+                if (predicate === 'OR') {
+                    return true;
+                }
+                found = true;
+            } else if (predicate === 'AND') {
+                return false;
+            }
+        }
+    }
+    return found;
+}
+
+function recursiveMatch(element, keyword) {
+    if (typeof element === 'object') {
+        if (Array.isArray(element)) {
+            for (var i = 0; i < element.length; i++) {
+                if (recursiveMatch(element[i], keyword)) {
+                    return true;
+                }
+            }
+        } else {
+            for (var i in element) {
+                if (recursiveMatch(element[i], keyword)) {
+                    return true;
+                }
+            }
+        }
+    } else if (typeof element === 'string') {
+        return element.includes(keyword);
+    } else if (typeof element === 'number') {
+        return String(element).includes(keyword);
+    }
+    return false;
+}
--- test/test.js
+++ test/test.js
@@ -1,9 +1,57 @@
 'use strict';
 
-var lib = require('..');
+var filter = require('..');
 
-describe('test', function () {
-    it('should be tested', function () {
-        (42).should.equal(42);
+var test = [{
+    a: 'a',
+    b: 'b',
+    c: ['c', 'd'],
+    d: {
+        e: 'e',
+        f: {
+            g: ['h']
+        }
+    }
+}];
+
+describe('filter', function () {
+    it('1 keyword, OR', function () {
+        assert({keywords: ['a']}, 1);
+        assert({keywords: ['a'], predicate: 'OR'}, 1);
+        assert({keywords: ['c'], predicate: 'OR'}, 1);
+        assert({keywords: ['d'], predicate: 'OR'}, 1);
+        assert({keywords: ['e'], predicate: 'OR'}, 1);
+        assert({keywords: ['h'], predicate: 'OR'}, 1);
+        assert({keywords: ['z'], predicate: 'OR'}, 0);
+    });
+
+    it('1 keyword, AND', function () {
+        assert({keywords: ['a'], predicate: 'AND'}, 1);
+        assert({keywords: ['c'], predicate: 'AND'}, 1);
+        assert({keywords: ['d'], predicate: 'AND'}, 1);
+        assert({keywords: ['e'], predicate: 'AND'}, 1);
+        assert({keywords: ['h'], predicate: 'AND'}, 1);
+        assert({keywords: ['z'], predicate: 'AND'}, 0);
+    });
+
+    it('2 keywords, OR', function () {
+        assert({keywords: ['a', 'b']}, 1);
+        assert({keywords: ['a', 'b'], predicate: 'OR'}, 1);
+        assert({keywords: ['c', 'e'], predicate: 'OR'}, 1);
+        assert({keywords: ['d', 'x'], predicate: 'OR'}, 1);
+        assert({keywords: ['x', 'e'], predicate: 'OR'}, 1);
+        assert({keywords: ['x', 'y'], predicate: 'OR'}, 0);
+    });
+
+    it('2 keywords, AND', function () {
+        assert({keywords: ['a', 'b'], predicate: 'AND'}, 1);
+        assert({keywords: ['c', 'e'], predicate: 'AND'}, 1);
+        assert({keywords: ['d', 'x'], predicate: 'AND'}, 0);
+        assert({keywords: ['x', 'e'], predicate: 'AND'}, 0);
+        assert({keywords: ['x', 'y'], predicate: 'AND'}, 0);
     });
 });
+
+function assert(options, length) {
+    filter(test, options).should.have.lengthOf(length);
+}
