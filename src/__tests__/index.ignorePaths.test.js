6000ef24444f19bfc004cd1de8e3f94eca525232
Luc Patiny <luc@patiny.com>
2021-10-07 22:09:59 +0200

    chore: improve test structure

--- src/__tests__/index.ignorePaths.test.js
+++ src/__tests__/index.ignorePaths.test.js
@@ -9,22 +9,20 @@ let test = [
   },
 ];
 
-describe('ignorePaths', () => {
-  it('string operators', () => {
-    assert({ keywords: ['e:2'] }, 1);
-    assert({ keywords: ['f:3'] }, 1);
-    assert({ keywords: ['h.e:2'] }, 1);
-    assert({ keywords: ['h.e:3'] }, 0);
+test('ignorePaths', () => {
+  assert({ keywords: ['e:2'] }, 1);
+  assert({ keywords: ['f:3'] }, 1);
+  assert({ keywords: ['h.e:2'] }, 1);
+  assert({ keywords: ['h.e:3'] }, 0);
 
-    assert({ keywords: ['h.e:2'], ignorePaths: ['e'] }, 0);
-    assert({ keywords: ['h.e:2'], ignorePaths: ['h.e'] }, 0);
-    assert({ keywords: ['h.e:2'], ignorePaths: ['i.e'] }, 1);
-    assert({ keywords: ['h.e:2'], ignorePaths: ['h.f'] }, 1);
-    assert({ keywords: ['h.e:2'], ignorePaths: ['i'] }, 1);
-    assert({ keywords: ['h.e:2'], ignorePaths: ['h'] }, 0);
-    assert({ keywords: ['i:jkl'], ignorePaths: ['h'] }, 1);
-    assert({ keywords: ['i:jkl'], ignorePaths: ['i'] }, 0);
-  });
+  assert({ keywords: ['h.e:2'], ignorePaths: ['e'] }, 0);
+  assert({ keywords: ['h.e:2'], ignorePaths: ['h.e'] }, 0);
+  assert({ keywords: ['h.e:2'], ignorePaths: ['i.e'] }, 1);
+  assert({ keywords: ['h.e:2'], ignorePaths: ['h.f'] }, 1);
+  assert({ keywords: ['h.e:2'], ignorePaths: ['i'] }, 1);
+  assert({ keywords: ['h.e:2'], ignorePaths: ['h'] }, 0);
+  assert({ keywords: ['i:jkl'], ignorePaths: ['h'] }, 1);
+  assert({ keywords: ['i:jkl'], ignorePaths: ['i'] }, 0);
 });
 
 function assert(options, length) {
