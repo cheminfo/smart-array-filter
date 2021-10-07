69c1e9ac3757dac25100813d7c7b57f63dd0df34
Luc Patiny <luc@patiny.com>
2021-10-07 07:59:16 +0200

    test: add testcase for ignorePaths jpath

--- src/__tests__/index.ignorePaths.test.js
+++ src/__tests__/index.ignorePaths.test.js
@@ -17,6 +17,9 @@ describe('ignorePaths', () => {
     assert({ keywords: ['h.e:3'] }, 0);
 
     assert({ keywords: ['h.e:2'], ignorePaths: ['e'] }, 0);
+    assert({ keywords: ['h.e:2'], ignorePaths: ['h.e'] }, 0);
+    assert({ keywords: ['h.e:2'], ignorePaths: ['i.e'] }, 1);
+    assert({ keywords: ['h.e:2'], ignorePaths: ['h.f'] }, 1);
     assert({ keywords: ['h.e:2'], ignorePaths: ['i'] }, 1);
     assert({ keywords: ['h.e:2'], ignorePaths: ['h'] }, 0);
     assert({ keywords: ['i:jkl'], ignorePaths: ['h'] }, 1);
