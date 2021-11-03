9d667365688278fa03b6b92ca5c24a6eb1c8ead7
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2021-11-03 22:07:31 +0100

    migration convertkeywords

--- src/utils/convertKeywordsToCriteria.ts
+++ src/utils/convertKeywordsToCriteria.ts
@@ -3,21 +3,34 @@ import escapeRegExp from 'lodash.escaperegexp';
 import getCheckNumber from './getCheckNumber';
 import getCheckString from './getCheckString';
 
+interface Criterion{
+    is: boolean|RegExp,
+    key: boolean|RegExp,
+    negate: boolean,
+    valueReg: boolean|undefined,
+    checkString?:boolean,
+    checkNumber?:boolean
+}
 /**
- * @param keywords
- * @param options
+ * ConvertKeywordsToCriteria.
+ *
+ * @param keywords - String.
+ * @param options -Options.
+ * @param options.insensitive - { [index:String] : boolean } }.
+ * @param options.pathAlias - String.
+ * @returns Criterion.
  */
-export default function convertKeywordsToCriteria(keywords, options) {
+export default function convertKeywordsToCriteria(keywords:string[], options:{ insensitive:string, pathAlias:{ [index:string] : boolean } }):Criterion[] {
   const { insensitive, pathAlias } = options;
   return keywords.map((keyword) => {
-    let criterion = {
+    let criterion:Criterion = {
       is: false,
       key: false,
       negate: false,
       valueReg: undefined,
     };
 
-    if (keyword.charAt(0) === '-') {
+    if (keyword.startsWith('-')) {
       criterion.negate = true;
       keyword = keyword.substring(1);
     }
@@ -52,11 +65,13 @@ export default function convertKeywordsToCriteria(keywords, options) {
 }
 
 /**
- * @param criterion
- * @param keyword
- * @param insensitive
+ * FillCriterion.
+ *
+ * @param criterion - Criterion.
+ * @param keyword - String.
+ * @param insensitive - String.
  */
-function fillCriterion(criterion, keyword, insensitive) {
+function fillCriterion(criterion:Criterion, keyword:string, insensitive:string) {
   criterion.checkString = getCheckString(keyword, insensitive);
   criterion.checkNumber = getCheckNumber(keyword);
 }
