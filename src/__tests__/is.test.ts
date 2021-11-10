36db729844c1df3fa2c92d5df719b6b029100032
Daniel Kostro <kostro.d@gmail.com>
2021-11-10 11:37:23 +0100

    test: add test with is when array

--- src/__tests__/is.test.ts
+++ src/__tests__/is.test.ts
@@ -15,40 +15,39 @@ let test = [
       },
       bool: true,
     },
+    // is criterion is not tested on array itself
+    arr: [],
   },
   {
     str: 'abc',
-    arr: ['1', '2'],
     bool: false,
     num: 0,
+    arr: [0],
   },
 ];
 
 describe('is', () => {
   it('positive is', () => {
     // check if some fields are truthy
-    assert({ keywords: ['is:obj1.obj2'] }, 1);
-    assert({ keywords: ['is:bool'] }, 2);
-    assert({ keywords: ['is:nested'] }, 1);
-    assert({ keywords: ['is:str'] }, 1);
-    // TODO: determine how this test should behave
-    // assert({ keywords: ['is:arr'] }, 1);
-    assert({ keywords: ['is:num'] }, 1);
-    assert({ keywords: ['is:arr'] }, 2);
+    assert({ keywords: ['is:obj1.obj2'] }, [1]);
+    assert({ keywords: ['is:bool'] }, [0, 1]);
+    assert({ keywords: ['is:nested'] }, [1]);
+    assert({ keywords: ['is:str'] }, [2]);
+    assert({ keywords: ['is:num'] }, [0]);
+    assert({ keywords: ['is:arr'] }, [0]);
   });
   it('negated is', () => {
     // check if some fields are falsy
-    assert({ keywords: ['-is:bool'] }, 1);
-    assert({ keywords: ['-is:str'] }, 2);
-    assert({ keywords: ['-is:num'] }, 2);
-    // TODO: determine how this test should behave
-    // assert({ keywords: ['-is:arr'] }, 1);
+    assert({ keywords: ['-is:bool'] }, [2]);
+    assert({ keywords: ['-is:str'] }, [0, 1]);
+    assert({ keywords: ['-is:num'] }, [1, 2]);
+    assert({ keywords: ['-is:arr'] }, [1, 2]);
   });
 });
 
 /**
  * @internal
  */
-function assert(options: OptionsType | undefined, length: number) {
-  expect(filter(test, options)).toHaveLength(length);
+function assert(options: OptionsType | undefined, matches: number[]) {
+  expect(filter(test, { ...options, index: true })).toStrictEqual(matches);
 }
